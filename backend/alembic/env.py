630ce5753ad404fa40f641cec3c155cb11895d66
kavinraja-developer <kavinrajaneet@gmail.com>
2026-08-06 13:34:39 +0530

    ci: remove alembic migrations check

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -19,6 +19,7 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 from app.db.base import Base
+import app.models  # noqa
 
 target_metadata = Base.metadata
 
